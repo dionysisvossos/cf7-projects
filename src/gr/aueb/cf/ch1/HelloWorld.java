d6e267ff14550242f465f1ad8eec74eb87e14a14
Dionysis.Vossos <dionysis.vossos@windowslive.com>
2024-12-10 20:10:39 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,20 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Prints Hello Coding Factory is
+ * standard output (console).
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        //Prints Hello Coding Factory
+
+        /*
+         *Prints in standard output
+         * "Hello Coding Factory"
+         */
+        System.out.println("Hello Coding Factory!"); // Prints Hello Coding Factory
+        System.out.print("Hello World!");
+        System.out.println();
+    }
+}
